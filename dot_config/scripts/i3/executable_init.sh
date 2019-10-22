e68292e4036e9c5ddbc9d0140eca2d6e8f28e353
Sinkerine <github@15cm.net>
2019-10-21 23:18:16 -0700

    Fix cursor size in QT Apps; Move out i3 init script

---
+++ dot_config/scripts/i3/executable_init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Fix cursor size under QT Apps(see ~/.config/kcminputrc)
+kcminit
+
+# Keyboard settings: repeat_delay repeat_rate
+xset r rate 400 24
+
+# Autostart applications
+copyq &
+guake &
+goldendict &
+keepassxc &
+syncthing-gtk &
+
+# Applet
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nm-applet &
+blueman-applet &
+xfce4-power-manager &
+pamac-tray &
+redshift-gtk &
+pasystray &
+
+# Essentials
+pulseaudio -k; exec --no-startup-id pulseaudio --start -D
+fcitx &
+xautolock -time 300 -locker blurlock &
+autorandr -l default &
+
+# Starting things with i3
+systemctl --user start xsession.target &
