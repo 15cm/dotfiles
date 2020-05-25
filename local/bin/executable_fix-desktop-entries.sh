95bf99d70daab1be435353f31be72a098b7a2d90
Sinkerine <git@15cm.net>
2020-05-25 12:02:53 -0700

    [fix-desktop] Minor updates

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -2,11 +2,10 @@
 app_folder="/usr/share/applications"
 
 # bluz-git
-sudo sed -i 's|\(ExecStart=\).*|\1/usr/lib/bluetoothd|' /usr/lib/systemd/system/bluetooth.service
+# sudo sed -i 's|\(ExecStart=\).*|\1/usr/lib/bluetoothd|' /usr/lib/systemd/system/bluetooth.service
 
 # Plex
-sudo rm -f $app_folder/plexmediaplayer.desktop
-sudo sed -i 's/Exec=\S*$/& --desktop --scale-factor=2/' $app_folder/plex-media-player.desktop
+sudo sed -i 's/--tv/--desktop/' $app_folder/plexmediaplayer.desktop
 
 sudo systemctl daemon-reload
 
