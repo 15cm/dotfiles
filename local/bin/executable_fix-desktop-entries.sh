0b44f2303477b085164339ee40b71f44182621ab
Sinkerine <git@15cm.net>
2020-09-12 20:56:57 -0700

    Force Plexmediaplayer to start with 2X scale

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -1,10 +1,5 @@
 #/bin/bash
 app_folder="/usr/share/applications"
 
-# bluz-git
-# sudo sed -i 's|\(ExecStart=\).*|\1/usr/lib/bluetoothd|' /usr/lib/systemd/system/bluetooth.service
-
 # Plex
-sudo sed -i 's/--tv/--desktop/' $app_folder/plexmediaplayer.desktop
-
-sudo systemctl daemon-reload
+sudo sed -i 's/plexmediaplayer$/& --scale-factor 2/' $app_folder/plexmediaplayer.desktop
