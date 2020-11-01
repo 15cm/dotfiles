cbe020ff61be2153de1e6e2a1a48aef1872e0a00
Sinkerine <git@15cm.net>
2020-11-01 14:55:39 -0800

    Fix fcitx env

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -3,3 +3,4 @@ app_folder="/usr/share/applications"
 
 # Plex
 sudo sed -i 's/plexmediaplayer$/& --scale-factor 2/' $app_folder/plexmediaplayer.desktop
+perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
