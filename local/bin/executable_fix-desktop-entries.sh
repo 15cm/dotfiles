821799ff333b061ae4dc2dfe7b843921c8d62b7e
Sinkerine <git@15cm.net>
2020-11-30 21:43:43 -0800

    Update day time screen temperatures; sudo when fixing .desktop

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -3,4 +3,6 @@ app_folder="/usr/share/applications"
 
 # Plex
 sudo sed -i 's/plexmediaplayer$/& --scale-factor 2/' $app_folder/plexmediaplayer.desktop
-perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
+
+# Emacs
+sudo perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
