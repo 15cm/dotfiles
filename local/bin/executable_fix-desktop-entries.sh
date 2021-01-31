f6feb505af99db5d06b0420f991cf79f0080bb57
Sinkerine <git@15cm.net>
2021-01-30 20:54:01 -0800

    Fix hdpi of Audacity desktop file

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -6,3 +6,6 @@ sudo sed -i 's/plexmediaplayer$/& --scale-factor 2/' $app_folder/plexmediaplayer
 
 # Emacs
 sudo perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
+
+# Audacity
+sudo perl -pi -e 's/(Exec=env)(?! GDK_SCALE)/$1 GDK_SCALE=2/' /usr/share/applications/audacity.desktop
