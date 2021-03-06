ab7f54e4e967e95b8bea977ec5e3d5c850664212
Sinkerine <git@15cm.net>
2021-03-05 18:49:10 -0800

    Fix hdpi settings

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -1,11 +1,5 @@
 #/bin/bash
 app_folder="/usr/share/applications"
 
-# Plex
-sudo sed -i 's/plexmediaplayer$/& --scale-factor 2/' $app_folder/plexmediaplayer.desktop
-
 # Emacs
 sudo perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
-
-# Audacity
-sudo perl -pi -e 's/(Exec=env)(?! GDK_SCALE)/$1 GDK_SCALE=2/' /usr/share/applications/audacity.desktop
