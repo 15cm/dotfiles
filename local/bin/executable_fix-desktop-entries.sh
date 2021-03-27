59db93ce41551bbba519aa140061d97e973a4d6b
Sinkerine <git@15cm.net>
2021-03-27 11:06:26 -0700

    Override QT refactor ratio of unsupported applications

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -2,4 +2,8 @@
 app_folder="/usr/share/applications"
 
 # Emacs
-sudo perl -pi -e 's/(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
+sudo perl -pi -e 's/^(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
+
+sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=0 /' /usr/share/applications/calibre-gui.desktop
+sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=1 /' /usr/share/applications/cura.desktop
+sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=1 /' /usr/share/applications/goldendict.desktop
