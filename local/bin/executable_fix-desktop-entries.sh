1270c4438721820dd4cb2b00d4e0130fa92b66e7
Sinkerine <git@15cm.net>
2021-04-18 16:37:46 -0700

    Override QT scale env of transgui

--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -5,5 +5,6 @@ app_folder="/usr/share/applications"
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
 
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=0 /' /usr/share/applications/calibre-gui.desktop
+sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=0 /' /usr/share/applications/transgui.desktop
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=1 /' /usr/share/applications/cura.desktop
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=1 /' /usr/share/applications/goldendict.desktop
