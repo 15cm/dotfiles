ca3e240e85b9eca2c9e89cc91347a784d19b8bd5
Sinkerine <git@15cm.net>
2021-12-19 17:01:40 -0800

    Remove LC treak of emacs

--- dot_config/scripts/i3/executable_start-emacs-one-instance.sh
+++ dot_config/scripts/i3/executable_start-emacs-one-instance.sh
@@ -3,5 +3,5 @@
 if [ -S ~/local/run/emacs/server ]; then
   emacsclient -s ~/local/run/emacs/server -c
 else
-  LC_CTYPE=zh_CN.UTF-8 emacs
+  emacs
 fi
--- local/bin/executable_fix-desktop-entries.sh
+++ local/bin/executable_fix-desktop-entries.sh
@@ -1,9 +1,6 @@
 #/bin/bash
 app_folder="/usr/share/applications"
 
-# Emacs
-sudo perl -pi -e 's/^(Exec=)(?!env)/$1env LC_CTYPE=zh_CN.UTF-8 /' /usr/share/applications/emacs.desktop
-
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=0 /' /usr/share/applications/calibre-gui.desktop
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=0 /' /usr/share/applications/transgui.desktop
 sudo perl -pi -e 's/^(Exec=)(?!env)/$1env QT_SCREEN_SCALE_FACTORS=1 /' /usr/share/applications/cura.desktop
