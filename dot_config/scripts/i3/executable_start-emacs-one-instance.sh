b90e6bf7e629b9e0735d6d1eb4cd1179999f3717
Sinkerine <git@15cm.net>
2022-01-16 22:05:49 -0800

    Add kbd for forcely starting new instance of Emacs

--- dot_config/scripts/i3/executable_start-emacs-one-instance.sh
+++ dot_config/scripts/i3/executable_start-emacs-one-instance.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ $# -eq 1 ] && [ $1 = "-f" ]; then
+  rm ~/local/run/emacs/server
+fi
+
 if [ -S ~/local/run/emacs/server ]; then
   emacsclient -s ~/local/run/emacs/server -c
 else
